4909b7f8c6de02c7449a46559b8dd60f36a4dff2
Joseph Avila <daniloxd98@icloud.com>
2019-10-19 23:19:20 -0500

    master

---
+++ predict.py
@@ -0,0 +1,38 @@
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+import keras
+
+from keras.models import load_model
+from keras.utils import CustomObjectScope
+
+from keras.initializers import glorot_uniform
+
+
+
+
+
+
+longitud, altura = 150, 150
+modelo = './modelo/modelo.h5'
+pesos_modelo = './modelo/pesos.h5'
+with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+    cnn = load_model(modelo)
+cnn.load_weights(pesos_modelo)
+
+def predict(file):
+  x = load_img(file, target_size=(longitud, altura))
+  x = img_to_array(x)
+  x = np.expand_dims(x, axis=0)
+  array = cnn.predict(x)
+  result = array[0]
+  answer = np.argmax(result)
+  if answer == 0:
+    print("pred: Fires")
+  elif answer == 1:
+    print("pred: Normal")
+  elif answer == 2:
+    print("pred: Smoke")
+
+  return answer
+predict("avenger.jpg")
+
